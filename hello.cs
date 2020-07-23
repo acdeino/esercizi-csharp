db90978c4404cc2f95a19ce4706a57e13c4c3830
namlose <ahbehnomics@gmail.com>
2020-07-23 18:07:51 +0100

    some experiments with the strings properties in C#

--- hello.cs
+++ hello.cs
@@ -7,4 +7,16 @@ Console.WriteLine(aFriend);
 aFriend2 = "Maira";
 Console.WriteLine(aFriend2);
 
-Console.Console.WriteLine("Hello, " + aFriend + " and " + aFriend2);
+Console.WriteLine("Hello, " + aFriend + " and " + aFriend2);
+// or similarly to Python:
+Console.WriteLine($"Hello, {aFriend} and {aFriend2}");
+
+// doing the same specifying the strings first, you would have something like:
+string firstFriend = "Maria";
+string secondFriend = "Steven";
+
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+//if we wanted count the length of the given strings, we could write:
+Console.WriteLine($"The name of {firstFriend} has {firstFriend.length} characters.");
+Console.WriteLine($"Conversely, the name of {secondFriend} has {secondFriend.length} characters.");
