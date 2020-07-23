fa3415827a3b9fa0faa9e6b3f3b68ee415ee7189
namlose <ahbehnomics@gmail.com>
2020-07-23 17:48:12 +0100

    Hello World from Console, without classes

---
+++ hello.cs
@@ -0,0 +1,10 @@
+Console.WriteLine("Hello World!");
+Console.WriteLine("\n");
+
+string aFriend("Bill");
+Console.WriteLine(aFriend);
+
+aFriend2 = "Maira";
+Console.WriteLine(aFriend2);
+
+Console.Console.WriteLine("Hello, " + aFriend + " and " + aFriend2);
